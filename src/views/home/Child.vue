6baa59785c99b43be7745d1f5a2c6bfb6c4266d7
MohammedAboAqlain <mo7mednafez@gmail.com>
2022-01-15 11:43:28 +0200

    update

---
+++ src/views/home/Child.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <div>Child component</div>
+  <p>my Name is {{myName}}</p>
+  <input type="text" v-model="name">
+  <button @click="$emit('changeName', name)">Change Name</button>
+</div>
+</template>
+
+<script>
+export default {
+    props:['myName'],
+    data(){
+        return {
+            name: ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/home/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <p>Home Page</p>
+    <Child myName="Mohammed" @changeName="changeName" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Child from './Child.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Child,
+  },
+  methods: {
+    changeName(val) {
+      console.log(val);
+    },
+  },
+};
+</script>
